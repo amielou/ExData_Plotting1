61582e15c6ed286124a51b84e13cca4aee214fb7
amielou <amie.louise@me.com>
2014-06-07 14:47:55 -0300

    upload new file

--- plot1.R
+++ plot1.R
@@ -1,10 +1,7 @@
-library(sqldf)
-
-myFile <- read.csv.sql("household_power_consumption.txt", header = TRUE, sep=";", sql = "select * from file where Date >= '01/01/2007' & Date <= '02/02/2007'")
-
-edit(myFile)
-rm(list=ls())
-hist(myFile$Global_active_power, main = "Global Active Power", 
-     xlab="Global Active Power (Kilowatts)", ylab="Frequency", col="red", breaks=c(0,2,4,6,8,10,12))
+myFile <- read.csv("household_power_consumption.txt", header = TRUE, sep=";", nrows=2076000)
+mySubset <- myFile[((myFile$Date) == "1/2/2007" | (myFile$Date) == "2/2/2007"),]
+rm(myFile)
+hist(as.numeric(mySubset$Global_active_power), main = "Global Active Power", 
+     xlab="Global Active Power (Kilowatts)", ylab="Frequency", col="red")
 dev.copy(png, file="plot1.png")
 dev.off()
