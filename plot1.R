316741c64453afdbbb2ee1f9cb93f99dbe024c72
amielou <amie.louise@me.com>
2014-06-08 10:05:40 -0300

    Delete plot1.R

--- plot1.R
+++
@@ -1,7 +0,0 @@
-myFile <- read.csv("household_power_consumption.txt", header = TRUE, sep=";", nrows=2076000)
-mySubset <- myFile[((myFile$Date) == "1/2/2007" | (myFile$Date) == "2/2/2007"),]
-rm(myFile)
-hist(as.numeric(mySubset$Global_active_power), main = "Global Active Power", 
-     xlab="Global Active Power (Kilowatts)", ylab="Frequency", col="red")
-dev.copy(png, file="plot1.png")
-dev.off()
